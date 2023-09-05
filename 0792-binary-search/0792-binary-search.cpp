61e0b527d6e312e62964683748c46f75d6b6430f
Manish Sharma <114284060+manishsarmaa@users.noreply.github.com>
2023-09-05 21:58:44 +0530

    Time: 33 ms (43.79%), Space: 27.7 MB (9.56%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+      int s=0;
+      int e=nums.size()-1;
+      int m=s+(e-s)/2;
+      int ans=-1; 
+      while(s<=e)
+      {
+          if(nums[m]==target)
+          {
+              ans=m;
+              e=m-1;
+          }
+          else if(target<nums[m])
+          {
+              e=m-1;
+          }
+          else if(target>nums[m])
+          {
+              s=m+1;
+          }
+          m=s+(e-s)/2;
+      }  
+      return ans;
+    }
+};
