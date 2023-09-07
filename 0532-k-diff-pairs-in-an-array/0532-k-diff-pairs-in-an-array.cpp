a87ffe4f9c54b03ce1cd222dcba1f00c5d7d7418
Manish Sharma <114284060+manishsarmaa@users.noreply.github.com>
2023-09-07 23:39:37 +0530

    Time: 17 ms (55.13%), Space: 13.1 MB (78.37%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-int bs(vector<int>&nums,int start, int x){
+int BinarySearch(vector<int>&nums,int start, int x){
     int end = nums.size()-1;
     while(start<=end){
         int mid = (start+end)/2;
@@ -23,7 +23,7 @@ int bs(vector<int>&nums,int start, int x){
         sort(nums.begin(),nums.end());
         set<pair<int,int>> ans;
         for(int i=0;i<nums.size();i++){
-            if(bs(nums,i+1,nums[i]+k)!=-1){
+            if(BinarySearch(nums,i+1,nums[i]+k)!=-1){
                 ans.insert({nums[i],nums[i]+k});
             }
         }
