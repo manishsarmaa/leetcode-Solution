e7e3fc45f5741070ea7332a28cb31cc7055b6ae8
Manish Sharma <114284060+manishsarmaa@users.noreply.github.com>
2023-09-08 12:48:21 +0530

    Time: 27 ms (88.34%), Space: 31.1 MB (90.16%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoptr(vector<int>& arr, int k, int x)
+    {
+        int l=0,h=arr.size()-1;
+        while(h-l>=k)
+        {
+            if(x-arr[l]>arr[h]-x)
+            {
+                l++;
+            }
+            else
+            {
+                h--;
+            }
+        }
+        return vector<int>(arr.begin()+l,arr.begin()+h+1);
+    }
+
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        return twoptr(arr,k,x);
+    }
+};
