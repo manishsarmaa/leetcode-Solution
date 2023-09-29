027579db10d4d84aaefc00197a0d57e1a3fe5ad4
Manish Sharma <114284060+manishsarmaa@users.noreply.github.com>
2023-09-29 11:37:08 +0530

    Time: 115 ms (82.11%), Space: 104.7 MB (5.31%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
-    bool isMonotonic(vector<int>& nums) {
-        int n=nums.size();
-        bool a=false,b=false;
-        for(int i=0;i<n-1;i++)
+    bool inc(vector<int>nums){
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            if(nums[i+1]>nums[i])
+                return false;
+        }
+          return true;   
+    }
+
+    bool dec(vector<int>nums){
+        for(int i=0;i<nums.size()-1;i++)
         {
             if(nums[i]>nums[i+1])
-            a=true;
-            else if(nums[i+1]>nums[i])
-            b=true;
-            if(a==true & b==true)
-            return false;
+                return false;
         }
-        return true;
+          return true;
+    }
+
+    bool isMonotonic(vector<int>& nums) {
+        return (inc(nums) || dec(nums)) ;
     }
 };
