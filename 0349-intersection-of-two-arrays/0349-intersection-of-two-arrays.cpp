feb9a355ca0bae5e939cdf40c7a3dbe8802eabbe
Manish Sharma <114284060+manishsarmaa@users.noreply.github.com>
2023-10-07 20:25:42 +0530

    Time: 3 ms (84.33%), Space: 10.4 MB (42.64%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,30 +1,3 @@
-// class Solution {
-// public:
-//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-//         vector<int>ans;
-//         // sort(nums1.begin(),nums1.end());
-//         // sort(nums2.begin(),nums2.end());
-//          for(int i=0;i<nums1.size();i++)
-//         {
-//             for(int j=0;j<nums2.size();j++)
-//             {
-//                     if(i > 0 && nums1[i]==nums1[i-1]){
-//                       i++;
-//                      continue;
-//                     }
-
-//                 if(nums1[i]==nums2[j])
-//                 {
-//                     ans.push_back(nums1[i]);
-//                     nums2[j]=-1;
-//                     break;
-//                 }
-//             }
-//         }
-//         //unique(ans.begin(),ans.end());
-//         return ans;
-//     }
-// };
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
@@ -36,18 +9,15 @@ public:
         sort(nums1.begin(),nums1.end());
         sort(nums2.begin(),nums2.end());
         while(i<n && j<m){
-            //this will help to remove the duplicate entries
-            if(i > 0 && nums1[i]==nums1[i-1]){
+             if(i > 0 && nums1[i]==nums1[i-1]){
                 i++;
                 continue;
             }
-            //helps to push the elements in the array
             if(nums1[i]==nums2[j]){
                 ans.push_back(nums1[i]);
                 i++;
                 j++;
             }
-            //increments if element of the other array is smaller
             else if(nums1[i]>nums2[j]){
                 j++;
             }
