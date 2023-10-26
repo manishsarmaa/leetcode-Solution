7021dacfdb62e8045d90b795dbe2868dd5a1de88
Manish Sharma <114284060+manishsarmaa@users.noreply.github.com>
2023-10-26 08:34:49 +0530

    Time: 3 ms (48.98%), Space: 6.8 MB (37.42%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size()-1;
+        int ans=0;
+        for(int i=n;i>=0;i--){
+            if(s[i]==' ' && ans>0)
+            return ans;
+
+            else if(s[i]!=' ')
+            ans++;
+        }
+        return ans;
+    }
+};
