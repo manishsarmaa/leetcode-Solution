1464c44cdf80936ea3f20b9c4ffb35ff16440fda
Manish Sharma <114284060+manishsarmaa@users.noreply.github.com>
2024-02-18 07:05:44 +0530

    Time: 0 ms (100.00%), Space: 41 MB (94.74%) - LeetHub

--- 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.java
+++ 1450-number-of-students-doing-homework-at-a-given-time/1450-number-of-students-doing-homework-at-a-given-time.java
@@ -1,11 +1,11 @@
 class Solution {
-public:
-    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
-          int count = 0 ;
-   for(int i = 0 ; i<startTime.size() ; i++)
-    {
-        if(startTime[i] <= queryTime && endTime[i] >= queryTime ) count++;
+    public int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count=0;
+        for(int i=0;i<startTime.length;i++){
+            if(startTime[i]<=queryTime && endTime[i]>=queryTime){
+                count++;
+            }
+        }
+        return count;
     }
-     return count;
-    }
-};
+}  
