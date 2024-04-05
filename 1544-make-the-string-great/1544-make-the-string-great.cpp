61eb3098612a3b790ee06a7710e96a6bb8f07543
Manish Sharma <114284060+manishsarmaa@users.noreply.github.com>
2024-04-05 07:45:22 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (64.31%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeGood(string s) {
+                int n=s.size();
+        string ans=string(1, s[0]);
+        for(int i=1; i<n; i++){
+            if (!ans.empty() && s[i]!=ans.back() 
+            && (s[i]&31)==(ans.back()&31))//Note 'a'='A'+32 1-bit different
+                ans.pop_back();
+            else 
+                ans.push_back(s[i]);
+        }
+        return ans;
+
+    }
+};
